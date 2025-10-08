77f639d6d57ef5fb18c577f260a5b2c3116f56c5
Fábio Almeida <teg.fabio.almeida@gamil.com>
2025-10-08 14:37:47 -0300

    V1: Scheduller

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1090,8 +1090,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   }
   /* USER CODE BEGIN Callback 1 */
 
-  if(htim->Instance == TIM3) {
-	  schedulerEngine();
+  if(htim == &htim3) {
+  	  schedulerEngine();
   }
 
   /* USER CODE END Callback 1 */
